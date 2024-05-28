9f2fc8e3b29ecc1773e2375d09bc0fe8ceea296b
Sarib <zama.sarib@gmail.com>
2024-05-28 12:55:14 +0530

    All functionality added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
-import os
+from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.logger import logging
 
-training_pipeline_config = TrainingPipelineConfig()
-data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-print(data_ingestion_config.__dict__)
+import os,sys
+
+try:
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
+except Exception as e:
+    print(e)
+    logging.exception(e)
